baec6bf2ac1fe6147ade17979e2d347690d4f4b4
Jon <jonatan.eseiza@tecso.coop>
2023-09-11 16:54:38 -0300

    fist commit

--- auth-service/src/main/java/com/jd/dev/app/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/jd/dev/app/auth/AuthServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@PropertySource("classpath:bootstrap.properties")
 public class AuthServiceApplication implements CommandLineRunner {
     private final BCryptPasswordEncoder passwordEncode;
 
--- users/src/main/resources/import.sql
+++ users/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, enabled, first_name, last_name, email, login_attempts) VALUES ('andres', '$2a$10$2Xrd6dvuzH5VJR0ME8.XjerYm7nTgfO9EV4wZ7qy1GE4FSxSDVzXK', 1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', 0);
-INSERT INTO users (username, password, enabled, first_name, last_name, email, login_attempts) VALUES ('jonaeseiza', '$2a$10$M2XWBTFFCKKE1OKHWkbW0ec91PauX0MCecKUNoSMxpCu9mi7cbwIy', 1, 'Jonatan', 'Eseiza', 'jona.eseiza@bolsadeideas.com', 0);
+INSERT INTO users (username, password, enabled, first_name, last_name, email, login_attempts) VALUES ('andres', '$2a$10$2Xrd6dvuzH5VJR0ME8.XjerYm7nTgfO9EV4wZ7qy1GE4FSxSDVzXK', true, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', 0);
+INSERT INTO users (username, password, enabled, first_name, last_name, email, login_attempts) VALUES ('jonaeseiza', '$2a$10$M2XWBTFFCKKE1OKHWkbW0ec91PauX0MCecKUNoSMxpCu9mi7cbwIy', true, 'Jonatan', 'Eseiza', 'jona.eseiza@bolsadeideas.com', 0);
 
 INSERT INTO roles (name) VALUES ('ROLE_USER');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
